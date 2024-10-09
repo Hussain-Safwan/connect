d4f3c3ff3111f1972ca6970983827ae98b8abbef
Hussain Safwan <safwan.du16@gmail.com>
2024-10-09 21:13:41 +0600

    [FE] logi route added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import ResponsiveAppBar from './components/ResponsiveAppBar';
-import Layout from './components/Layout';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <Layout />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import "../styles/login.css";
+
+function Login() {
+  return <div className="login">Login page</div>;
+}
+export default Login;
