0ff150021811576efeac7c63686f92638647c081
Hussain Safwan <safwan.du16@gmail.com>
2024-10-10 18:40:29 +0600

    [BE] username already taken check

--- routes.js
+++ routes.js
@@ -42,12 +42,22 @@ routes.post("/login", passport.authenticate("local"), (req, res) => {
   res.json(req.user);
 });
 
-routes.post("/save", (req, res) => {
+routes.post("/save", async (req, res) => {
   const { name, phone, username, password } = req.body;
-  const user = new userModel({ name, phone, username, password });
-  user.save();
-  console.log("user saved");
-  res.status(200).send({ success: true, data: user });
+
+  const existingUser = await userModel.findOne({ username });
+  if (existingUser) {
+    res
+      .status(404)
+      .send({ success: false, message: "Username already taken", data: null });
+  } else {
+    const user = new userModel({ name, phone, username, password });
+    user.save();
+    console.log("user saved");
+    res
+      .status(200)
+      .send({ success: true, message: "Account created", data: user });
+  }
 });
 
 module.exports = routes;
