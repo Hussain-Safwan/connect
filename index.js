9c29737b27605aa502f5ee3d5b4560e0e11e831e
Hussain Safwan <safwan.du16@gmail.com>
2024-10-14 12:35:02 +0600

    [BE] depl: '/' routed to client index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
-const passport = require("passport");
+const path = require("path");
 const cors = require("cors");
 
 app = express();
@@ -21,13 +21,17 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // deployment test
-app.get("/", (req, res) =>
-  res.status(200).json({ message: "deployment successful!" })
+app.get("/hello", (req, res) =>
+  res.status(200).json({ message: "hello, the deployment successful!" })
 );
+
 app.use("/api/", routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 4000;
