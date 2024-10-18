86a8cf59d1b990ecfaa1ef9dbfc0962b652cfd2b
Hussain Safwan <safwan.du16@gmail.com>
2024-10-18 21:42:02 +0600

    [BE] server socket connection

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const http = require("http");
 const mongoose = require("mongoose");
 const express = require("express");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
+const socket = require("socket.io");
+const { sendMessage } = require("./messageController");
 
 app = express();
+const server = http.Server(app);
+const io = socket(server, { cors: { origin: "*" } });
 
 const uri =
   "mongodb+srv://safwandu16:home761049@connect-cluster-01.87zba.mongodb.net/?retryWrites=true&w=majority&appName=connect-cluster-01";
@@ -35,4 +40,11 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Server is running at port: ${port}`));
+server.listen(port, () => console.log(`Server is running at port: ${port}`));
+
+io.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    console.log(message);
+    sendMessage(message);
+  });
+});
