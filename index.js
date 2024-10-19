304300f525a9b8c9e11b9f4d7b8770f0b8eafaba
Hussain Safwan <safwan.du16@gmail.com>
2024-10-19 11:45:38 +0600

    [BE] changed CORS settings, might not work

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
 const socket = require("socket.io");
-const { sendMessage } = require("./messageController");
+const { sendMessage, addThread } = require("./messageController");
 
 app = express();
-const server = http.Server(app);
-const io = socket(server, { cors: { origin: "*" } });
 
 const uri =
   "mongodb+srv://safwandu16:home761049@connect-cluster-01.87zba.mongodb.net/?retryWrites=true&w=majority&appName=connect-cluster-01";
@@ -22,12 +20,13 @@ mongoose
   })
   .then(() => console.log("mongoose connected"));
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.options("*", cors());
 app.use(bodyParser.json());
 
 // deployment test
 app.get("/hello", (req, res) =>
-  res.status(200).json({ message: "hello, the deployment successful!" })
+  res.status(200).json({ message: "hello, the deployment was successful!" })
 );
 
 app.use("/api", routes);
@@ -40,11 +39,21 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const port = process.env.PORT || 4000;
-server.listen(port, () => console.log(`Server is running at port: ${port}`));
+const server = http.createServer(app);
+const io = socket(server, { cors: { origin: "*" } });
+server.listen(port, () =>
+  console.log(`Realtime server is running at port: ${port}`)
+);
 
 io.on("connection", (socket) => {
   socket.on("message", async (message) => {
     const thread = await sendMessage(message);
     io.emit("message", thread);
+  });
+
+  socket.on("add-thread", async (body) => {
+    console.log(body);
+    const res = await addThread(body);
+    io.emit("add-thread", res);
   });
 });
